ddf44dc6cf17d5520c8d0eb36221294a4eac5381
Prog-LucasAlves <lucasalves_taz@hotmail.com>
2023-08-08 20:23:14 -0300

    📦 NEW: 003

---
+++ app2.py
@@ -0,0 +1,19 @@
+##################################
+# Bibliotecas/Pacotes importadas #
+##################################
+import streamlit as st
+
+##################################
+# Inicio da Construção Streamlit #
+##################################
+
+######
+st.set_option("deprecation.showPyplotGlobalUse", False)
+
+st.set_page_config(layout="wide")
+
+# CURRENT_THEME = "light"
+# IS_DARK_THEME = False
+
+# Cabeçalho da página - Informações de fechamento de alguns Ìndices
+st.subheader("🌎 Alguns Índices Globais")
